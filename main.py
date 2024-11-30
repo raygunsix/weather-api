bc16eb65ddc6254bfdf57ae01b629c0ca9afd562
Chris Reid <chris@raygun6.com>
2024-11-29 17:43:58 -0800

    add home page

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask("Website")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
+app.run(debug=True)
