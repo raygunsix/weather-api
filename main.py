9c2b5b96e19ddd9a0072ff2680ac2150b0a6018c
Chris Reid <chris@raygun6.com>
2024-11-30 18:10:36 -0800

    return correct temperature data

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) +".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
